8394a9693603fc2fd09f79ab807b344d8576fcc9
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2021-12-25 11:22:40 +0530

    test contract to be called from multsig contract after txn execution

---
+++ contracts/TestContract.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract TestContract {
+    uint public i;
+
+    function callMe(uint _j) public {
+        i += _j;
+    }
+
+    function getData() public pure returns (bytes memory) {
+        return abi.encodeWithSignature("callMe(uint256)", 123);
+    }
+}
